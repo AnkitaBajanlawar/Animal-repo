01fa45e94e8d8cea1d0732df57446cf2ca066479
Ankita Bajanlawar <ankitabajanlawar@gmail.com>
2024-09-17 18:27:17 +0530

    Submmited

---
+++ src/JSONServerBookApp.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import BookCreate from './components/BookCreate';
+import BookList from './components/BookList';
+ 
+const JSONServerBookApp = () => {
+    return (
+        <div className="app">
+            <h1>BookApp with JSON Server as DB</h1>
+            {/* <OtherContext>
+                <Advertisement />
+            </OtherContext> */}
+            {/* <Provider> */}
+            <BookList />
+            <BookCreate />
+            {/* </Provider> */}
+ 
+        </div>
+    );
+}
+ 
+export default JSONServerBookApp
